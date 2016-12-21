bed752bbe165803a70b37b5678f0e4d35f27b452
James Kerak <jkerak@001830-mac.local>
2016-12-21 16:39:14 -0500

    add spring boot maven project as created from spring initializr

---
+++ src/main/java/com/jkerak/GithubExplorerApiApplication.java
@@ -0,0 +1,12 @@
+package com.jkerak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubExplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubExplorerApiApplication.class, args);
+	}
+}
